6bd36f21eccada9c923d9b3a3143a02cf6ef7dbb
nao20010128nao <nao20010128@gmail.com>
2018-07-15 14:16:08 +0900

    変な事した

--- src/main/kotlin/com/nao20010128nao/Cryptorage/FileSourceContractSpecs.kt
+++ src/main/kotlin/com/nao20010128nao/Cryptorage/FileSourceContractSpecs.kt
@@ -1,5 +1,5 @@
 package com.nao20010128nao.Cryptorage
 
 enum class FileSourceContractSpecs(val hasMultipleAddDel: Boolean = false, val hasRangeListAndRemoveAt: Boolean = false) {
-    NON_EXISTENT, V1(false), V2(true, true)
+    NON_EXISTENT, V1(false), V2(true), V3(true, true)
 }
